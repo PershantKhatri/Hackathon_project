6e7e16c77ffff3f97c99f6946a711b6606586fc4
PershantKhatri <kparshant525@gmail.com>
2026-09-01 20:13:28 +0500

    .

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -4,12 +4,13 @@ const API = axios.create({
   baseURL: 'https://hackathon-project-khatri6.vercel.app',
 });
 
-// Request interceptor to attach JWT token automatically
 API.interceptors.request.use((req) => {
   const token = localStorage.getItem('token');
+
   if (token) {
     req.headers.Authorization = `Bearer ${token}`;
   }
+
   return req;
 });
 
