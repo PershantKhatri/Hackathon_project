9c69d315864a735b07169c8d7f4d69fb5ddcaa9b
PershantKhatri <kparshant525@gmail.com>
2026-09-01 21:19:42 +0500

    .

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: 'https://hackathon-project-khatri6.vercel.app', // Live Vercel backend URL
+  baseURL: 'http://localhost:5000', // Local backend URL
 });
 
 API.interceptors.request.use((req) => {
